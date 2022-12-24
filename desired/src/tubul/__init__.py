209ad153ccfde583c18b9227baaa1b64948bd010
Alessandro Candido <candido.ale@gmail.com>
2022-12-24 10:20:01 +0100

    Add desired layout

---
+++ desired/src/tubul/__init__.py
@@ -0,0 +1,14 @@
+from functools import reduce
+
+import tphon
+
+
+def show():
+    return list(filter(lambda m: not m.startswith("_"), dir(tphon)))
+
+
+def sum(*args):
+    if len(args) == 1:
+        return sum(*args[0])
+
+    return reduce(lambda x, y: tphon.sum_as_string(int(x), y), args, "0")
